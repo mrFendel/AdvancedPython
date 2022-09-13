755ada22b89875917274d72ed5f4fe59a3d04f91
mrFendel <novikov.ivan@phystech.edu>
2022-09-14 02:43:30 +0300

    Changes: ================ - filled setup.py in STEM ________________

---
+++ stem_framework/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AdvancedPython',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='mrFendel',
+    author_email='novkov.ivan@physech.edu',
+    description='AdvancedPythonCourse'
+)
