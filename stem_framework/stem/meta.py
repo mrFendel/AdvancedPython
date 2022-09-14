e2f26eafdee023a767a2030344e985f69754f1cf
mrFendel <novikov.ivan@phystech.edu>
2022-09-14 17:58:44 +0300

    Changes: ================ - sphinx and setup.py integration - added docstrings to stem package - added autodoc and built documentation ________________

--- stem_framework/stem/meta.py
+++ stem_framework/stem/meta.py
@@ -1,43 +1,10 @@
-from dataclasses import dataclass
-from typing import Optional, Any, Union
+"""
+Metadata:
+    Is data that contains adjoint information to other data.
 
-
-Meta = ... # TODO()
-
-SpecificationField = ... # TODO()
-
-Specification = ... # TODO()
-
-
-class SpecificationError(Exception):
-    pass
-
-
-@dataclass
-class MetaFieldError:
-    required_key: str
-    required_types: Optional[tuple[type]] = None
-    presented_type: Optional[type] = None
-    presented_value: Any = None
-
-
-class MetaVerification:
-
-    def __init__(self, *errors: Union[MetaFieldError, "MetaVerification"]):
-        self.error = errors
-        # TODO("checked_success")
-
-    @staticmethod
-    def verify(meta: Meta,
-               specification: Optional[Specification] = None) -> "MetaVerification":
-        ...  # TODO()
-
-
-def get_meta_attr(meta : Meta, key : str, default : Optional[Any] = None) -> Optional[Any]:
-    ...
-    # TODO()
-
-
-def update_meta(meta: Meta, **kwargs):
-    ...
-    # TODO()
+Metadata processing:
+    This principle postulates that, during the
+processing of data, only initial data and it's metadata are allowed to be used as input.
+This means no user instructions (scripts) or
+manually managed intermediate states are possible.
+"""
