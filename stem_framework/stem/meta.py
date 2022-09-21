53a767c9b5808e911e1251237d14fe90546d28fd
mrFendel <novikov.ivan@phystech.edu>
2022-09-21 23:59:12 +0300

    Changes: ================ - made 1-4 and 6 tasks - made 5.i task ________________

--- stem_framework/stem/meta.py
+++ stem_framework/stem/meta.py
@@ -1,10 +1,63 @@
-"""
-Metadata:
-    Is data that contains adjoint information to other data.
+from dataclasses import dataclass, is_dataclass
+from typing import Optional, Any, Union, Type, Tuple
+from stem_framework.stem.core import Dataclass
 
-Metadata processing:
-    This principle postulates that, during the
-processing of data, only initial data and it's metadata are allowed to be used as input.
-This means no user instructions (scripts) or
-manually managed intermediate states are possible.
-"""
+
+Meta = Union[dict, Dataclass]
+
+SpecificationField = Tuple[object, Union[Type, Tuple[Type, ...], Type[Meta]]]
+
+Specification = Union[Type[Dataclass], Type[SpecificationField]]
+
+
+class SpecificationError(Exception):
+    pass
+
+
+@dataclass
+class MetaFieldError:
+    required_key: str
+    required_types: Optional[tuple[type]] = None
+    presented_type: Optional[type] = None
+    presented_value: Any = None
+
+
+class MetaVerification:
+
+    def __init__(self, *errors: Union[MetaFieldError, "MetaVerification"]):
+        self.error = errors
+
+    @property
+    def checked_success(self):
+        flag = True
+        for err in self.error:
+            if isinstance(err, MetaFieldError):
+                flag = False
+                break
+        return flag
+
+    @staticmethod
+    def verify(meta: Meta,
+               specification: Optional[Specification] = None) -> "MetaVerification":
+
+
+def get_meta_attr(meta: Meta, key: str, default: Optional[Any] = None) -> Optional[Any]:
+    if type(meta) is dict:
+        try:
+            return meta[key]
+        except KeyError:
+            return default
+    else:
+        try:
+            return getattr(meta, key)
+        except AttributeError:
+            return default
+
+
+def update_meta(meta: Meta, **kwargs):
+    if type(meta) is dict:
+        for keyword, arg in kwargs.items():
+            meta[keyword] = arg
+    else:
+        for keyword, arg in kwargs.items():
+            setattr(meta, keyword, arg)
