35ec12dcf02146f2a7c810ce7c36915a2cdb15aa
mrFendel <novikov.ivan@phystech.edu>
2022-10-11 02:48:15 +0300

    Updated workspace file template

--- stem_framework/stem/workspace.py
+++ stem_framework/stem/workspace.py
@@ -1,7 +1,120 @@
-"""
-Modularity:
-    The core of the system contains only basic functionality
-for working with metadata and organizing the data processing,
-all specific functions, such as the graphical environment
-or tools for statistical analysis, are placed in separate plug-ins.
-"""
+from abc import abstractmethod, ABC, ABCMeta
+from types import ModuleType
+from typing import Optional, Any, TypeVar, Union
+
+from .core import Named
+from .meta import Meta
+from .task import Task
+
+T = TypeVar("T")
+
+
+class TaskPath:
+    def __init__(self, path: Union[str, list[str]]):
+        if isinstance(path, str):
+            self._path = path.split(".")
+        else:
+            self._path = path
+
+    @property
+    def is_leaf(self):
+        return len(self._path) == 1
+
+    @property
+    def sub_path(self):
+        return TaskPath(self._path[1:])
+
+    @property
+    def head(self):
+        return self._path[0]
+
+    @property
+    def name(self):
+        return self._path[-1]
+
+    def __str__(self):
+        return ".".join(self._path)
+
+
+class ProxyTask(Task[T]):
+
+    def __init__(self, proxy_name, task: Task):
+        self._name = proxy_name
+        self._task = task
+
+    @property
+    def dependencies(self):
+        return self._task.dependencies
+
+    @property
+    def specification(self):
+        return self._task.specification
+
+    def check_by_meta(self, meta: Meta):
+        self._task.check_by_meta(meta)
+
+    def transform(self, meta: Meta, /, **kwargs: Any) -> T:
+        return self._task.transform(meta, **kwargs)
+
+
+class IWorkspace(ABC, Named):
+
+    @property
+    @abstractmethod
+    def tasks(self) -> dict[str, Task]:
+        pass
+
+    @property
+    @abstractmethod
+    def workspaces(self) -> set["IWorkspace"]:
+        pass
+
+    def find_task(self, task_path: Union[str, TaskPath]) -> Optional[Task]:
+        ...  # TODO()
+
+    def has_task(self, task_path: Union[str, TaskPath]) -> bool:
+        return self.find_task(task_path) is not None
+
+    def get_workspace(self, name) -> Optional["IWorkspace"]:
+        for workspace in self.workspaces:
+            if workspace.name == name:
+                return workspace
+        return None
+
+    def structure(self) -> dict:
+        return {
+            "name": self.name,
+            "tasks": list(self.tasks.keys()),
+            "workspaces": [w.structure() for w in self.workspaces]
+        }
+
+    @staticmethod
+    def find_default_workspace(task: Task) -> "IWorkspace":
+        ...  # TODO()
+
+    @staticmethod
+    def module_workspace(module: ModuleType) -> "IWorkspace":
+        ...  # TODO()
+
+
+class ILocalWorkspace(IWorkspace):
+
+    @property
+    def tasks(self) -> dict[str, Task]:
+        return self._tasks
+
+    @property
+    def workspaces(self) -> set["IWorkspace"]:
+        return self._workspaces
+
+
+class LocalWorkspace(ILocalWorkspace):
+
+    def __init__(self, name,  tasks=(), workspaces=()):
+        self._name = name
+        self._tasks = tasks
+        self._workspaces = workspaces
+
+
+class Workspace(ABCMeta, ILocalWorkspace):
+    ...  # TODO()
