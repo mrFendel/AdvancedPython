741f538b05642bdf9d561c75665b245cb8078243
mrFendel <novikov.ivan@phystech.edu>
2022-12-14 00:47:58 +0300

    Task 12

---
+++ temperature_monitor/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from sphinx.setup_command import BuildDoc
+
+cmdclass = {'build_sphinx': BuildDoc}
+name = 'AdvancedPython'
+version = '0.1'
+
+setup(
+    name=name,
+    version=version,
+    author='mrFendel',
+    cmdclass=cmdclass,
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author_email='novkov.ivan@physech.edu',
+    description='AdvancedPythonCourse',
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'source_dir': ('setup.py', 'doc'),
+            },
+        'console_scripts': [
+            'run = stem.app:run'
+        ]
+    }
+)
